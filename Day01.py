6cc52577d571ce750fb2b7500623cbdf9b05ee2c
Jason Choi <99243083+JaewooJason@users.noreply.github.com>
2022-07-16 13:42:45 +0900

    백준 파이썬 문제(2)

--- Day01.py
+++ Day01.py
@@ -1,10 +1,16 @@
+# 고양이 만들기
 print('\\    /\\')
 print(" )  ( ')")
 print('(  /  )')
 print(' \\(__)|')
-
+# 강아지 만들기
+print('*'*30)
 print('|\\_/|')
 print('|q p|   /}')
 print('( 0 )"""\\')
 print('|"^"`    |')
 print('||_/=\\\\__|')
+# A+B
+a,b = map(int, input().split())
+print(a+b)
+
