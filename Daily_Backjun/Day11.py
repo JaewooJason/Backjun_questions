d54346cbc86dfeeeaf7e8b6b6b54e09aa99fe7a3
Jason Choi <99243083+JaewooJason@users.noreply.github.com>
2022-07-29 17:34:49 +0900

    백준 파이썬 문제 day11(1)

---
+++ Daily_Backjun/Day11.py
@@ -0,0 +1,13 @@
+# 1546 문제 평균
+
+n = int(input())
+score = list(map(int,input().split()))
+max_score = max(score)
+
+new_list =[]
+
+for i in score:
+    new_list.append(score/max_score*100)
+new_avg = sum(new_list)/n
+
+print(new_avg)
