3d8f54dc4c84d40178e14e823af2785001797fd6
Jason Choi <99243083+JaewooJason@users.noreply.github.com>
2022-08-08 15:18:23 +0900

    백준 파이썬 문제 day16 (2)

--- Daily_Backjun/Day16.py
+++ Daily_Backjun/Day16.py
@@ -4,3 +4,27 @@ n = input().split()
 print(len(n))
 
 
+# 상수
+# 이 문제에서 중요한것은 숫자를 역순으로 바꾸는것이다.
+a, b  = input().split()
+a = int(a[::-1]) # 이렇게 슬라이싱을 이용하면 들어온 숫자를 역순으로 변경할 수 있다.
+b= int(b[::-1])
+
+if a > b:
+    print(a)
+else:
+    print(b)
+
+# 5622 문제 다이얼
+
+dial = ['ABC','DEF', 'GHI','JKL','MNO','PQRS','TUV', 'WXYZ']
+
+num = list(input())
+
+time = 0
+
+for i in num:
+    for j in dial:
+        if i in j :
+            time += dial.index(j) + 3
+print(time)
