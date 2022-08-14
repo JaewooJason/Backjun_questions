c1905761d83ea8c8833e95403d1797670c85d534
Jason Choi <99243083+JaewooJason@users.noreply.github.com>
2022-08-14 20:22:50 +0900

    백준 파이썬 문제 day20 (2)

--- Daily_Backjun/Day20.py
+++ Daily_Backjun/Day20.py
@@ -3,10 +3,10 @@
 t = int(input())
 
 for i in range(t):
-    floor = int(input())
-    num = int(input())
-    base = [x for x in range(1, num+1)]
-    for j in range(floor):
-        for k in range(1, num):
-            base[k] += base[k-1]
-        print(base[-1])
+    f = int(input())
+    n = int(input())
+    b = [x for x in range(1, n+1)]
+    for j in range(f):
+        for k in range(1, n):
+            b[k] += b[k-1]
+    print(b[-1]) # 인덴트를 신경써야 한다.
