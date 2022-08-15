1f8ddd193b5d9ac007bf4808f14fd6c844dbfc95
Jason Choi <99243083+JaewooJason@users.noreply.github.com>
2022-08-15 19:28:00 +0900

    백준 파이썬 문제 day20 (3)

--- Daily_Backjun/Day20.py
+++ Daily_Backjun/Day20.py
@@ -10,3 +10,17 @@ for i in range(t):
         for k in range(1, n):
             b[k] += b[k-1]
     print(b[-1]) # 인덴트를 신경써야 한다.
+
+# 2839 설탕배달
+
+n = int(input())
+bag = 0
+while n >= 0 :
+    if n %5 ==0: # 5의 배수이면
+        bag += (n//5) # 5로 나눈 몫을 구해야 정수가 된다
+        print(bag)
+        break
+    n -= 3
+    bag +=1  # 5의 배수일때 설탕-3, 봉지+1
+else:
+    print(-1)
