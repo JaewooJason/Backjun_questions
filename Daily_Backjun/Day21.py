b75a3e82402ce6f00d1c8dc732c5dc94d68798cd
Jason Choi <99243083+JaewooJason@users.noreply.github.com>
2022-08-17 22:54:49 +0900

    백준 파이썬 문제 day21 (2)

--- Daily_Backjun/Day21.py
+++ Daily_Backjun/Day21.py
@@ -26,3 +26,25 @@ for nums in num:
     if cnt2==1:
         cnt1 +=1
 print(cnt1)
+
+#2581 소수 찾기 2
+
+s_num = int(input())
+e_num = int(input())
+
+sosu = []
+
+for num in range(s_num, e_num +1):
+    error = 0
+    if num > 1:
+        for i in range(2, num):
+            if num % i == 0:
+                error += 1
+                break
+        if error == 0:
+            sosu.append(num)
+if len(sosu)>0:
+    print(sum(sosu))
+    print(min(sosu))
+else:
+    print(-1)
