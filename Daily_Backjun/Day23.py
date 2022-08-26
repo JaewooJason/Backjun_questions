cca13d2bcc72cca41958a976fb8fd2d7559e78bb
Jason Choi <99243083+JaewooJason@users.noreply.github.com>
2022-08-26 21:42:11 +0900

    백준 파이썬 문제 day23 (2)

--- Daily_Backjun/Day23.py
+++ Daily_Backjun/Day23.py
@@ -25,3 +25,44 @@ while True:
     print(cnt)
     n = int(input())
 
+# 9020 골드바흐의 추축
+
+def gold(n):
+    if n ==1:
+        return False
+    for i in range(2,int(n**0,5)+1):
+        if n % i ==0:
+            return False
+    return True
+for _ in range(int(input())):
+    num = int(input())
+    a,b = num//2, num//2
+    while a>0:
+        if gold(a) and gold(b):
+            print(a,b)
+        else:
+            a -= 1
+            b += 1
+
+import sys
+
+tmp_list= [False,False] +[True]*10002
+
+for i in range(2, 10002):
+    if tmp_list[i]:
+        for j in range(2*i, 10002, i):
+            tmp_list[j]= False
+t = int(sys.stdin.readline())
+
+for y in range(t):
+    n = int(sys.stdin.readline())
+    a = n//2
+    b = a
+    while a > 0 :
+        if tmp_list[a] and tmp_list[b]:
+            print(a,b)
+            break
+        else:
+            a-=1
+            b+=1
+
